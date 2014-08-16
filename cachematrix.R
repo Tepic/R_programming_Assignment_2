2e06735f7e76b1b152755480d3dd7acdfaf575c0
Tepic <tepic.milan@yahoo.com>
2014-08-16 20:46:25 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -11,26 +11,34 @@
 # cacheSolve function will according to "tf" give the inverse matrix from the cache or do the inversion.
 # After doing the inversion of the new matrix, then it would be listed in caches.
 
-counter<-0
+#counter<-0
 # counter of the position of specific matrix
 
-cache_matrix<-list()
+#cache_matrix<-list()
 # cache for original matrix
 
-cache_solution<-list()
+#cache_solution<-list()
 # cache for inverse matrix of matrix from cache_matrix
+
+rm(list=ls()) # cleaning the memory with variables
 
 makeCacheMatrix<-function(target)
 {
+	if(!exists("counter"))
+	{
+		counter<<-0
+		cache_matrix<<-list()
+		cache_solution<<-list()
+	}
 	tf<-0			
 	# initializing variable "tf" to false (0) as it was inputed new matrix, which don not exist in the cache
 	
 	position<-0
 	# initializing position of the matrix to 0 in the list (where the matrix was found), as it was not found
 
-	if(counter)
+	if(counter) # if counter=0, then cache is empty and checking for identical matrix is skiped
 	{
-		for(i in 1:counter)
+		for(i in 1:counter) # going through cache to check if there is identical matrix
 			{
 				checkin<-identical(target,cache_matrix[[i]])
 				# checking if there is given matrix already calculated
@@ -50,7 +58,7 @@ makeCacheMatrix<-function(target)
 	# after (not) finding the position of already calculated matrix
 	# function cacheSolve is being called
 	result<-cacheSolve(target,tf,position)
-	result
+	print(result)
 
 	tf<-0
 	position<-0
@@ -58,26 +66,27 @@ makeCacheMatrix<-function(target)
 }
 
 cacheSolve<-function(target,tf,position)
+{
+	if(tf)
 	{
-		if(tf)
-		{
-			h<-c(cache_solution[position])
-			solution<-h[[1]]
-			# if it was calculated, then as solution it gets calculation from cache
-		}
-
-		else
-		{
-			solution<-solve(target)
-			h<-counter
-			counter<<-h+1
-			cache_solution[[counter]]<<-solution
-			cache_matrix[[counter]]<<-target
-
-			# if it wasn not calculated, then it is being calculated here
-			# and its solution is being stored in cache of solutions as well
-			# as original matrix in cache of original matrix
-		}
-		casheSolve<-solution
-		# returning the cached/calculated result
+		h<-c(cache_solution[position])
+		solution<-h[[1]]
+		message("Getting from the cache...")
+		# if it was calculated, then as solution it gets calculation from cache
 	}
+
+	else
+	{
+		solution<-solve(target)
+		counter<<-counter+1
+		cache_solution[[counter]]<<-solution
+		cache_matrix[[counter]]<<-target
+		message("Calculating...")
+
+		# if it wasn not calculated, then it is being calculated here
+		# and its solution is being stored in cache of solutions as well
+		# as original matrix in cache of original matrix
+	}
+	casheSolve<-solution
+	# returning the cached/calculated result
+}
